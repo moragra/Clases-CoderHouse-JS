c71389319304dc47543eb7f0c7ec1fae3346a0f6
moragra <moragra_98@outlook.com>
2022-08-10 20:10:03 -0600

    desafio clase 4

--- 4ta Clase-JS/Desafio/desafio.js
+++ 4ta Clase-JS/Desafio/desafio.js
@@ -1,1 +1,29 @@
+let product = prompt("Input a name of a product")
+let price_product = parseInt(prompt("Input the price of " + product))
+let num_product = parseInt(prompt("Input the amount of " + product + "s you want to buy"))
 
+function total_cost (price_product, num_product){
+    let cost = price_product * num_product
+    return cost
+}
+
+let promo_code_Q = prompt("Have a promo code? Y/N")
+
+function promo_code (promo_code_Q){
+    if (promo_code_Q == "y" || promo_code_Q == "Y"){
+        let code = prompt("Enter the promo code")
+        if (code == "AAA"){
+            let promo_applied = total_cost(price_product, num_product) * 0.30
+            let total = total_cost(price_product, num_product) - promo_applied
+            return total
+        } else {
+            alert("Invalid promo code!")
+            return total_cost(price_product, num_product)
+        }
+    } else {
+        return total_cost(price_product, num_product)
+    }
+}
+
+console.log("Your total cost is: " + total_cost(price_product, num_product))
+console.log("Your total cost with the promo code is: " + promo_code(promo_code_Q))
