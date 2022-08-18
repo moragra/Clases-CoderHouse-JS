e4d0b82698ccf7aaa96f3daab5ad433e7eb39173
Graciela Mora Mejias <105560476+moragra@users.noreply.github.com>
2022-08-18 17:14:40 -0600

    fix deasfio clase 6

--- 6ta Clase-JS/Desafio/desafio.js
+++ 6ta Clase-JS/Desafio/desafio.js
@@ -18,52 +18,49 @@ for (let i = 1; i <= amount_purchase; i++) {
     name_product = prompt("Input the name of product " + i)
     price_product = parseFloat(prompt("Input the unit price of " + name_product))
     num_product = parseInt(prompt("Input the amount of " + name_product + "s you want to purchase"))
-
-    let new_product = new products(name_product, price_product, num_product)
-    products_array.push(new_product)
     
-}
+    let new_product_array = new products(name_product, price_product, num_product)
+    products_array.push(new_product_array)
+    
+}  let cost = 0
 
 for (let product of products_array) {
     console.log("<----------------------->")
     console.log("Name of product:",product.name_product)
     console.log("Amount:", product.num_product)
     console.log("Price per unit:", product.price_product)
+    console.log("Total price for", product.name_product, "is:", (product.price_product*product.num_product))
+	
+    cost = (product.price_product*product.num_product) + cost  
 }
 
 //1.2/3 Finaliza desafío complementario clase 6
 
-function total_cost (price_product, num_product){
-    let cost = price_product * num_product
-    return cost
-}
-
 let promo_code_Q = prompt("Have a promo code? Y/N")
 
-function promo_code (promo_code_Q){
+function promo_code (cost, promo_code_Q){
     if (promo_code_Q == "y" || promo_code_Q == "Y"){
         let code = prompt("Enter the promo code")
         if (code == "AAA"){
-            let promo_applied = total_cost(price_product, num_product) * 0.30
-            let total = total_cost(price_product, num_product) - promo_applied
+            let promo_applied = cost * 0.30
+            let total = cost - promo_applied
             console.log("<----------------------->")
             console.log("Your total cost with the promo code is: " + total)
             return total
         } else {
             alert("Invalid promo code!")
             console.log("<----------------------->")
-            console.log("Your total cost is: " + total_cost(price_product, num_product))
-            return total_cost(price_product, num_product)
+            console.log("Your total cost is: " + cost)
+            return cost
         }
     } else {
         console.log("<----------------------->")
-        console.log("Your total cost is: " + total_cost(price_product, num_product))
-        return total_cost(price_product, num_product)
+        console.log("Your total cost is: " + cost)
+        return cost
     }
 }
 
-total_cost(price_product, num_product)
-promo_code(promo_code_Q)
+promo_code(cost, promo_code_Q)
 
 //1.3/3 Empieza desafío complementario clase 6
 
